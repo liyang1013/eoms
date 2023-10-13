658688a13ba9f034e13035f4defb6ad5928f5f52
JX2302304 <Leo.Li@keboda.com>
2023-10-13 14:29:47 +0800

    服务挂了检测小车情况

--- eoms-back/src/main/java/com/keboda/eomsback/timer/RcsTimer.java
+++ eoms-back/src/main/java/com/keboda/eomsback/timer/RcsTimer.java
@@ -3,6 +3,9 @@ package com.keboda.eomsback.timer;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.json.JSONUtil;
+import com.keboda.eomsback.plc.enums.SiemensPLC;
+import com.keboda.eomsback.plc.pojo.Car;
+import com.keboda.eomsback.plc.utils.ReadCarUtils;
 import com.keboda.eomsback.utils.SocketUtils;
 import com.keboda.eomsback.utils.WeChatPlusUtils;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -14,13 +17,7 @@ import java.util.TreeMap;
 
 @Component
 public class RcsTimer {
-
-    private static final String URL = "http://172.17.200.154:8090/rest/robots/states";
-
-    private static final String authorization = "mrbase64 mrrest:YWRtaW4mYWRtaW4=";
-
     private static final String sender = "JX2302304|JX2001001|JX1907006";
-
 
     /**
      * 检查RCS可用性，发送提醒
@@ -31,39 +28,25 @@ public class RcsTimer {
         boolean mysql = SocketUtils.isReachable("172.17.200.154", 3306);
         boolean back = SocketUtils.isReachable("172.17.200.154", 8090);
         boolean front = SocketUtils.isReachable("172.17.200.154", 8081);
-        StringBuilder sb  = new StringBuilder();
 
         if (!(mysql && back && front)) {
+            StringBuilder sb  = new StringBuilder();
             sb.append("未检测到RCS（172.17.200.154）服务运行: ");
-            sb.append("mysql(3306): ").append(mysql ? " 已开启" : " 未开启").append("\n");
-            sb.append("back(8090): ").append(back ? " 已开启" : " 未开启").append("\n");
-            sb.append("front(8081): ").append(front ? " 已开启" : " 未开启");
+            sb.append("mysql(3306): ").append(mysql ? " 运行中" : " 未运行").append("\n");
+            sb.append("back(8090): ").append(back ? " 运行中" : " 未运行").append("\n");
+            sb.append("front(8081): ").append(front ? " 运行中" : " 未运行");
 
             WeChatPlusUtils.sendMessage(sender, sb.toString());
-            return;
-        }
 
-        SortedMap<Object, Object> sortedMap = new TreeMap<Object, Object>() {
-            private static final long serialVersionUID = 1L;
+            sb.setLength(0);
+            for (SiemensPLC plc : SiemensPLC.values()) {
+                if (plc.name().contains("Car")) {//小车
 
-            {
-                ArrayList<Integer> id = new ArrayList<>();
-                put("params", id);
+                    Byte id = ReadCarUtils.getSxcId(plc.getSiemensS7Net());
+                    sb.append("Car").append(id).append(": ").append(id != -1 ? "在线": "离线").append("\n");
+                }
             }
-        };
-        HttpResponse res = null;
-        try {
-            res = HttpRequest.post(URL)
-                    .header("authorization", authorization)
-                    .timeout(2000)
-                    .body(JSONUtil.toJsonStr(sortedMap))
-                    .execute();
-        } catch (Exception e) {
-            WeChatPlusUtils.sendMessage(sender, "RCS接口 172.17.200.154:8090/rest/robots/states 异常: " + e.getMessage());
-            return;
-        }
-        if (res.getStatus() != 200) {
-            WeChatPlusUtils.sendMessage(sender, "RCS接口 172.17.200.154:8090/rest/robots/states 异常: " + res.getStatus());
+            WeChatPlusUtils.sendMessage(sender, sb.toString());
         }
     }
 }
